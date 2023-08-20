677f297ddc091f8b6509b640aaf2562cc6622688
Katherine Clark <60265279+KClarkDev@users.noreply.github.com>
2023-08-19 20:51:06 -0400

    Successfully running category PUT requests

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,78 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// GET all categories and associated products
+router.get("/", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// GET one category by its ID value, along with its associated products
+router.get("/:id", async (req, res) => {
+  try {
+    const categoryID = req.params.id;
+    const categoryData = await Category.findByPk(categoryID, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "Category not found" });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// POST (create) a new category
+router.post("/", async (req, res) => {
+  try {
+    // Retrieve data from the request body
+    const { category_name } = req.body;
+
+    // Create a new category using Sequelize
+    const newCategory = await Category.create({
+      category_name: category_name,
+    });
+
+    // Respond with the newly created category
+    res.status(201).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// PUT (update} a category by its ID value
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.json(updatedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
 });
 
